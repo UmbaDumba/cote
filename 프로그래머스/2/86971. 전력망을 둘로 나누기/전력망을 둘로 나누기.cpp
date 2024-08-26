92324f89f48b0a7910e920f68b01a635471032c1
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2024-08-26 23:46:52 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.44 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -0,0 +1,92 @@
+#include <string>
+#include <vector>
+#include <climits>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int find_seed(vector<vector<int>> wires){
+    for(int i = 0; i<wires.size(); i++){
+        for(int j = 0; j<wires.size(); j++){
+            if(wires[i][j] == 1){
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int tree_count_sub(vector<vector<int>> wires){
+    
+    
+    // 첫번째 트리 개수 찾기
+    int count1 = 1;
+    queue<int> que;
+    que.push(0);
+    while(!que.empty()){
+        int node = que.front();
+        que.pop();
+        for(int i = 0; i<wires.size(); i++){
+            if(wires[node][i] == 1){
+                // 연결된곳을 찾은것
+                wires[node][i] = 0;
+                wires[i][node] = 0;
+                que.push(i);
+                count1++;
+            }
+        }
+        
+    }
+    
+    //cout << "1 : "<<count1<<endl;
+    
+    // 두번째 트리 개수 찾기
+    int count2 = 1;
+    queue<int> que2;
+    int n = find_seed(wires);
+    que2.push(n);
+    if(n == -1){
+        return INT_MAX;
+    }
+    
+    while(!que2.empty()){
+        int node = que2.front();
+        que2.pop();
+        for(int i = 0; i<wires.size(); i++){
+            if(wires[node][i] == 1){
+                // 연결된곳을 찾은것
+                wires[node][i] = 0;
+                wires[i][node] = 0;
+                que2.push(i);
+                count2++;
+            }
+        }
+        
+    }
+    
+    return abs(count1 - count2);
+}
+
+int solution(int n, vector<vector<int>> wires) {
+    int answer = INT_MAX;
+    
+    vector<vector<int>> trees(n, vector<int>(n, 0));
+    
+    for(int i = 0; i<wires.size(); i++){
+        trees[wires[i][0]-1][wires[i][1]-1] = 1;
+        trees[wires[i][1]-1][wires[i][0]-1] = 1;
+    }
+    
+    for(int i = 0; i<wires.size(); i++){
+        vector<vector<int>> test = trees;
+        test[wires[i][0]-1][wires[i][1]-1] = 0;
+        test[wires[i][1]-1][wires[i][0]-1] = 0;
+        // 끊기
+        
+        answer = min(answer, tree_count_sub(test));
+        
+    }
+    
+    return answer;
+}
