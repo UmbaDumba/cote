d5eb8d937be21a2ee4dd50a1e81b0da1294f3e45
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2025-06-10 11:34:43 +0900

    [level 1] Title: 폰켓몬, Time: 0.77 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,28 +1,17 @@
 #include <vector>
-#include <unordered_map>
-#include <algorithm>
-#include <iostream>
+#include <unordered_set>
 using namespace std;
 
 int solution(vector<int> nums)
 {
     int answer = 0;
     
-    unordered_map<int, int> pokets;
-    for(int i = 0; i<nums.size(); i++)
-    {
-        if(pokets.find(nums[i]) == pokets.end())
-        {
-            answer++;
-            pokets[nums[i]] = 1;
-        }
-        // 개수가 중요하진 않음
-        
-    }
+    unordered_set<int> pokets(nums.begin(), nums.end());
+    
     
     //cout << answer << endl;
     
-    answer = min(answer, ((int)nums.size()) / 2);
+    answer = min(((int)nums.size() / 2), (int)pokets.size());
     
     return answer;
 }
