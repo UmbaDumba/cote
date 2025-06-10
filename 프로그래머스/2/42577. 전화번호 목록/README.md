5561b26a494691e56d9b43ea8e3814d27e71aa52
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2025-06-10 11:50:55 +0900

    [level 2] Title: 전화번호 목록, Time: 81.24 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 35.6 MB, 시간: 87.10 ms
+메모리: 31.5 MB, 시간: 81.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 26일 12:14:57
+2025년 06월 10일 11:50:54
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,31 +1,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     
-    for(int i = 0; i<phone_book.size();i++){
-        phone_book[i].erase(remove(phone_book[i].begin(), phone_book[i].end(), ' '), phone_book[i].end());
-    }
-    
     sort(phone_book.begin(), phone_book.end());
     
-    for(int i = 0; i<phone_book.size()-1;i++){
-        if(phone_book[i].size() <= phone_book[i+1].size()){
-            if(!phone_book[i].compare(phone_book[i+1].substr(0, phone_book[i].size()))){
-                return false;
-            }
-        }
-        if(phone_book[i+1].size() <= phone_book[i].size()){
-            if(!phone_book[i+1].compare(phone_book[i].substr(0, phone_book[i+1].size()))){
-                return false;
-            }
+    int compj = 0;
+    for(int i = 1; i<phone_book.size(); i++)
+    {
+        if(phone_book[i].compare(0, phone_book[compj].size(), phone_book[compj]) == 0)
+        {
+            // 접두어
+            return false;
+        }else{
+            compj = i;
         }
     }
-    
     
     
     return true;
