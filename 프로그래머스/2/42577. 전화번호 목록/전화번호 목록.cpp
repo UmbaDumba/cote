41ef893531b1e8abe89f85915b47dcbf3fbb3d72
UmbaDumba <88480816+UmbaDumba@users.noreply.github.com>
2025-06-10 12:24:43 +0900

    [level 2] Title: 전화번호 목록, Time: 224.98 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,25 +1,24 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     
-    sort(phone_book.begin(), phone_book.end());
+    unordered_set<string> set(phone_book.begin(), phone_book.end());
     
-    int compj = 0;
-    for(int i = 1; i<phone_book.size(); i++)
+    for(int i = 0; i<phone_book.size(); i++)
     {
-        if(phone_book[i].compare(0, phone_book[compj].size(), phone_book[compj]) == 0)
+        for(int j = 1; j<phone_book[i].size(); j++)
         {
-            // 접두어
-            return false;
-        }else{
-            compj = i;
+            if(set.find(phone_book[i].substr(0, j)) != set.end())
+            {
+                // 접두어 있음
+                return false;
+            }
         }
     }
-    
     
     return true;
 }
